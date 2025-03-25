a6c54e976546eb2c4d2932e2f749da273b98240c
Yannick Debrée <yannick.debree.ext@ovhcloud.com>
2025-03-25 18:19:25 +0100

    Style specifications improved

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,11 @@
 import '!style-loader!css-loader!sass-loader!./main.scss';
-import type { Preview } from "@storybook/web-components";
+import type { Preview } from '@storybook/web-components';
+
+document.querySelector('html')?.classList.add('antheme-html');
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
--- src/styles/base/typography.stories.ts
+++ src/styles/base/typography.stories.ts
@@ -7,6 +7,9 @@ export const Typography = {
 <h1>This is a h1</h1>
 <h2>This is a h2</h2>
 <h3>This is a h3</h3>
+<h4>This is a h4</h4>
+<h5>This is a h5</h5>
+<h6>This is a h6</h6>
 <p>This is a <strong>paragraph</strong></p>
 <p>This is a <a href="">link</a></p>
 <q>This is a quote</q>`,
